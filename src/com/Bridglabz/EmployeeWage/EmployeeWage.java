ea9930d6b416d32029cbd8bd4806513669a977cb
kuldeepkumar17feb@gmail.com <kuldeepkumar17feb@gmail.com>
2023-12-07 09:27:40 +0530

    Added[Kuldeep]:Employee Condition Present or absent

--- src/com/Bridglabz/EmployeeWage/EmployeeWage.java
+++ src/com/Bridglabz/EmployeeWage/EmployeeWage.java
@@ -1,7 +1,17 @@
 package com.Bridglabz.EmployeeWage;
-
+/*
+@Desc: Employee wage problem,
+       Use case one check Employee is present or absent.
+@Params: Variable  number of employee
+ */
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to employee Wage...");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is not present");
+        }
     }
 }
