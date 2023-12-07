695542e44e3c3a5b5680c4a12838fac8a902a176
kuldeepkumar17feb@gmail.com <kuldeepkumar17feb@gmail.com>
2023-12-07 09:36:06 +0530

    Added[Kuldeep]:Part time Employee

--- src/com/Bridglabz/EmployeeWage/EmployeeWage.java
+++ src/com/Bridglabz/EmployeeWage/EmployeeWage.java
@@ -1,22 +1,30 @@
 package com.Bridglabz.EmployeeWage;
 /*
 @Desc: Employee wage problem,
-       Use case one check Employee is present or absent.
+       Use case one check Employee is present or absent.,
+       Daily Employee Wage,
+       Part time Employee Wagew.
 @Params: Variable  number of employee
  */
 public class EmployeeWage {
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
+        //Constant variables
+        int IS_PART_TIME=1;
+        int IS_FULL_TIME = 2;
         int empRate_Per_Hour=30;
+        //Variables
         int empHours=0;
         int empWage=0;
+        //Condition
         double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME) {
+        if (empCheck == IS_PART_TIME)
+            empHours=4;
+        else if(empCheck==IS_FULL_TIME)
             empHours=8;
-        } else {
+        else
             empHours=0;
-            empWage=empHours*empRate_Per_Hour;
-            System.out.println("EmployeeWage: "+empWage);
-        }
+        empWage=empHours*empRate_Per_Hour;
+        System.out.println("EmployeeWage: "+empWage);
+
     }
 }
