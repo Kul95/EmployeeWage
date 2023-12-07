b06d7b52a6e3240c7b65a35ef37df50fd68fe847
kuldeepkumar17feb@gmail.com <kuldeepkumar17feb@gmail.com>
2023-12-07 09:40:37 +0530

    Added[Kuldeep]:Solve using Switch case

--- src/com/Bridglabz/EmployeeWage/EmployeeWage.java
+++ src/com/Bridglabz/EmployeeWage/EmployeeWage.java
@@ -7,24 +7,19 @@ package com.Bridglabz.EmployeeWage;
 @Params: Variable  number of employee
  */
 public class EmployeeWage {
+    public  static  final  int IS_PART_TIME=1;
+    public  static  final  int IS_FULL_TIME=2;
+    public  static  final int EMP_RATE_PER_HOUR=30;
     public static void main(String[] args) {
-        //Constant variables
-        int IS_PART_TIME=1;
-        int IS_FULL_TIME = 2;
-        int empRate_Per_Hour=30;
-        //Variables
         int empHours=0;
         int empWage=0;
-        //Condition
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_PART_TIME)
-            empHours=4;
-        else if(empCheck==IS_FULL_TIME)
-            empHours=8;
-        else
-            empHours=0;
-        empWage=empHours*empRate_Per_Hour;
-        System.out.println("EmployeeWage: "+empWage);
-
+        int empCheck=(int)Math.floor(Math.random()*10)%3;
+        switch (empCheck){
+            case IS_PART_TIME:empHours=4;break;
+            case IS_FULL_TIME:empHours=8;break;
+            default:empHours=0;
+        }
+        empWage=empHours*EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage: "+empWage);
     }
 }
